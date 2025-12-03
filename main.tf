ddf2a116dd34eb61e0eaf5a510583ae6f000096c
DuckOriDuck <seocd777@gmail.com>
2025-12-03 19:54:57 +0900

    fix: hard-rollback

--- main.tf
+++ main.tf
@@ -358,12 +358,6 @@ module "ssm" {
       type        = "String"
       description = "K3s cluster initialization status"
     }
-    k3s_control_plane_ip = {
-      name        = "/k3s/control-plane-ip"
-      value       = "placeholder"
-      type        = "String"
-      description = "K3s control plane private IP (updated by control plane)"
-    }
   }
 
   tags = var.tags
@@ -495,8 +489,8 @@ module "k3s_worker" {
   max_size               = var.worker_max_size
   target_cpu_utilization = var.worker_target_cpu
   root_volume_size       = 30
-  server_private_ip      = var.enable_compute_instance ? module.k3s_control_plane[0].private_ip : ""
-  user_data = templatefile("${path.module}/userdata/k3s-worker.sh.tpl", {
+  server_private_ip      = var.enable_compute_instance ? module.k3s_control_plane[0].private_ip : "" # This might be used by the asg module itself, but not for the template
+  user_data              = templatefile("${path.module}/userdata/k3s-worker.sh.tpl", {
     server_private_ip = var.enable_compute_instance ? module.k3s_control_plane[0].private_ip : ""
   })
 
--- userdata/k3s-worker.sh.tpl
+++ userdata/k3s-worker.sh.tpl
@@ -30,11 +30,7 @@ while true; do
 done
 
 
-SERVER_IP=$(aws ssm get-parameter \
-  --region "ap-northeast-2" \
-  --name "/k3s/control-plane-ip" \
-  --query "Parameter.Value" \
-  --output text)
+SERVER_IP="${server_private_ip}"
 SERVER_URL="https://${SERVER_IP}:6443"
 
 TOKEN=$(aws ssm get-parameter \
